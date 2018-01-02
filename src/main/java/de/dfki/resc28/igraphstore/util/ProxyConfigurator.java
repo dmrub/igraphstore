f323351d430520eef09f7d57517e47e097d9cf7d
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2018-01-03 00:34:34 +0100

    Added support for enabling insecure SSL connections.
    Disable SSL checks by setting ssl.insecure system property to true or 1,
    e.g. -Dssl.insecure=true

--- src/main/java/de/dfki/resc28/igraphstore/util/ProxyConfigurator.java
+++ src/main/java/de/dfki/resc28/igraphstore/util/ProxyConfigurator.java
@@ -11,6 +11,14 @@ import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import org.apache.http.HttpHost;
 import org.apache.http.config.Registry;
@@ -20,15 +28,18 @@ import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.jena.riot.web.HttpOp;
+import org.apache.http.ssl.SSLContextBuilder;
 
 /**
  *
- * @author rubinste
+ * @author Dmitri Rubinstein
  */
 public class ProxyConfigurator {
 
@@ -43,7 +54,14 @@ public class ProxyConfigurator {
         }
     }
 
-    static class MyConnectionSocketFactory extends PlainConnectionSocketFactory {
+    static class MyPlainConnectionSocketFactory extends PlainConnectionSocketFactory {
+
+        @SuppressWarnings("FieldNameHidesFieldInSuperclass")
+        public static final MyPlainConnectionSocketFactory INSTANCE = new MyPlainConnectionSocketFactory();
+
+        public static MyPlainConnectionSocketFactory getSocketFactory() {
+            return INSTANCE;
+        }
 
         @Override
         public Socket createSocket(final HttpContext context) throws IOException {
@@ -68,9 +86,8 @@ public class ProxyConfigurator {
 
     static class MySSLConnectionSocketFactory extends SSLConnectionSocketFactory {
 
-        public MySSLConnectionSocketFactory(final SSLContext sslContext) {
-            // You may need this verifier if target site's certificate is not secure
-            super(sslContext, ALLOW_ALL_HOSTNAME_VERIFIER);
+        public MySSLConnectionSocketFactory(SSLContext sslContext, HostnameVerifier hostnameVerifier) {
+            super(sslContext, hostnameVerifier);
         }
 
         @Override
@@ -94,22 +111,95 @@ public class ProxyConfigurator {
         }
     }
 
+    // code based on http://literatejava.com/networks/ignore-ssl-certificate-errors-apache-httpclient-4-4/
+    public static CloseableHttpClient createHttpClient(boolean insecureSSL, boolean useSocks) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        if (!insecureSSL && !useSocks) {
+            return HttpClients.createDefault();
+        }
+
+        final SSLContextBuilder sslB = SSLContextBuilder.create();
+        final HostnameVerifier hostnameVerifier;
+        if (insecureSSL) {
+            // don't check Hostnames, either.
+            hostnameVerifier = NoopHostnameVerifier.INSTANCE;
+
+            // setup a Trust Strategy that allows all certificates.
+            //
+            sslB.loadTrustMaterial(null, new TrustStrategy() {
+                @Override
+                public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                    return true;
+                }
+            });
+        } else {
+            hostnameVerifier = SSLConnectionSocketFactory.getDefaultHostnameVerifier();
+        }
+
+        final SSLContext sslContext = sslB.build();
+
+        final SSLConnectionSocketFactory sslSocketFactory;
+        final PlainConnectionSocketFactory plainSocketFactory;
+
+        // here's the special part:
+        //      -- need to create an SSL Socket Factory, to use our weakened "trust strategy";
+        //      -- and create a Registry, to register it.
+        //
+        if (useSocks) {
+            plainSocketFactory = MyPlainConnectionSocketFactory.getSocketFactory();
+            sslSocketFactory = new MySSLConnectionSocketFactory(sslContext, hostnameVerifier);
+        } else {
+            plainSocketFactory = PlainConnectionSocketFactory.getSocketFactory();
+            sslSocketFactory = new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
+        }
+
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", plainSocketFactory)
+                .register("https", sslSocketFactory)
+                .build();
+
+        // now, we create connection-manager using our Registry.
+        //      -- allows multi-threaded use
+        PoolingHttpClientConnectionManager connMgr
+                = new PoolingHttpClientConnectionManager(
+                        socketFactoryRegistry,
+                        useSocks ? new FakeDnsResolver() : null);
+
+        // finally, build the HttpClient;
+        //      -- done!
+        return HttpClientBuilder.create().setSSLContext(sslContext)
+                .setConnectionManager(connMgr).build();
+    }
+
     public static CloseableHttpClient createHttpClient() {
         String socksProxyHost = System.getProperty("socksProxyHost");
         String socksProxyPort = System.getProperty("socksProxyPort");
+        String insecureSSLValue = System.getProperty("ssl.insecure");
+        boolean insecureSSL = ("true".equalsIgnoreCase(insecureSSLValue)
+                || "1".equals(insecureSSLValue));
+        boolean useSocks = socksProxyHost != null && socksProxyPort != null;
 
-        if (socksProxyHost != null && socksProxyPort != null) {
-            System.out.format("Detected SOCKS configuration:%nsocksProxyHost=%s%nsocksProxyPort=%s%nConfigure proxy support", socksProxyHost, socksProxyPort);
-
-            Registry<ConnectionSocketFactory> reg = RegistryBuilder.<ConnectionSocketFactory>create()
-                    .register("http", new MyConnectionSocketFactory())
-                    .register("https", new MySSLConnectionSocketFactory(SSLContexts.createSystemDefault())).build();
-            PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(reg, new FakeDnsResolver());
-
-            return HttpClients.custom().setConnectionManager(cm).build();
-        } else {
-            return HttpClients.createDefault();
+        if (insecureSSL) {
+            System.err.format("WARNING: Enabled insecure SSL/TLS%n");
         }
+
+        if (useSocks) {
+            System.out.format("Detected SOCKS configuration:%nsocksProxyHost=%s%nsocksProxyPort=%s%nConfigure proxy support", socksProxyHost, socksProxyPort);
+        }
+
+        try {
+            return createHttpClient(insecureSSL, useSocks);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(ProxyConfigurator.class.getName()).log(
+                    Level.SEVERE, "Could not create HTTP client with insecure SSL", ex);
+        } catch (KeyStoreException ex) {
+            Logger.getLogger(ProxyConfigurator.class.getName()).log(
+                    Level.SEVERE, "Could not create HTTP client with insecure SSL", ex);
+        } catch (KeyManagementException ex) {
+            Logger.getLogger(ProxyConfigurator.class.getName()).log(
+                    Level.SEVERE, "Could not create HTTP client with insecure SSL", ex);
+        }
+
+        return HttpClients.createDefault();
     }
 
     public static void initHttpClient() {
